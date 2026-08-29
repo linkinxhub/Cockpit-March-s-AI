e38b58025ea07c3e5e774f034efc35c1c2e8ef45
linkinxhub <89360758+linkinxhub@users.noreply.github.com>
2026-08-29 04:41:23 +0200

    Start Flutter behind secure mobile connection gate

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'app_shell.dart';
+import 'features/auth/presentation/mobile_connection_gate.dart';
 
 const apiBaseUrl=String.fromEnvironment('API_BASE_URL',defaultValue:'http://localhost:3000');
 
-void main()=>runApp(const CockpitMarketsApp(apiBaseUrl:apiBaseUrl));
+void main()=>runApp(const MobileConnectionGate(apiBaseUrl:apiBaseUrl));
